1e58a202fba94597b9b417738698af323d0392c5
Abdul Ahad <ahadfsd28@gmail.com>
2025-09-01 20:59:51 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const repoName = "mothers-meals-nextjs-website";
 const nextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
-  trailingSlash: true, // 🔑 important for GitHub Pages
+//   trailingSlash: true, // 🔑 important for GitHub Pages
 };
 
 export default nextConfig;
