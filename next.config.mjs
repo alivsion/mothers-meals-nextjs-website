df01538ca5c5439d5fae1ea06fc4392d7fdead7b
Abdul Ahad <ahadfsd28@gmail.com>
2025-09-03 16:47:55 +0530

    feat: update GitHub Actions workflow to build Next.js into docs folder and commit changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const repoName = process.env.GITHUB_REPOSITORY
   ? process.env.GITHUB_REPOSITORY.split('/')[1] // take only repo name
-  : ''; // empty if not building for GitHub Pages
+  : '';
 
 const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig = {
-  output: 'export',
+  output: 'export', 
+  distDir: 'docs',  // ✅ export into `docs` instead of `.next` or `out`
   images: {
     unoptimized: true,
   },
