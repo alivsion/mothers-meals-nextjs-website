d8520e4f4690fd284f2a5291bd697185ad514979
Abdul Ahad <ahadfsd28@gmail.com>
2025-09-03 19:10:35 +0530

    feat: remove GitHub Actions deployment workflow and update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
-const repoName = process.env.GITHUB_REPOSITORY
-  ? process.env.GITHUB_REPOSITORY.split('/')[1] // take only repo name
-  : '';
-
-const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig = {
-  output: 'export', 
-  distDir: 'docs',  // ✅ export into `docs` instead of `.next` or `out`
-  images: {
-    unoptimized: true,
-  },
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+ 
 };
 
 export default nextConfig;
